8e19a202f9fdc56af7e186ac7ff6f5fbdedd7030
Alessandro Ballerini <alessandroballerini95@gmail.com>
2022-05-11 18:50:17 +0200

    Fixing emotion decoder

--- src/MVAE.py
+++ src/MVAE.py
@@ -29,47 +29,49 @@ class ProductOfExperts(nn.Module):
      
         
 class ImageEncoder(nn.Module):
-    def __init__(self, z_dim=512, ch=32, scale=2):
+    def __init__(self, z_dim=64, ch=64, scale=2):
         super(ImageEncoder, self).__init__()
         self.ch = ch
+        self.z_dim = z_dim
+        self.features_output = self.ch * 8 * 2 * 2
         
         # input = 64 * 64
         self.features = nn.Sequential(
             nn.Conv2d(3, ch, 3, 1, 1, bias=False), nn.BatchNorm2d(ch), nn.ReLU(),
-            nn.AvgPool2d(scale, scale), # 32
+            nn.MaxPool2d(scale, scale), # 32
             nn.Conv2d(ch, ch * 2, 3, 1, 1, bias=False), nn.BatchNorm2d(ch * 2), nn.ReLU(),
-            nn.AvgPool2d(scale, scale), # 16
+            nn.MaxPool2d(scale, scale), # 16
             nn.Conv2d(ch * 2, ch * 4, 3, 1, 1, bias=False), nn.BatchNorm2d(ch * 4), nn.ReLU(),
-            nn.AvgPool2d(scale, scale), # 8
+            nn.MaxPool2d(scale, scale), # 8
             nn.Conv2d(ch * 4, ch * 8, 3, 1, 1, bias=False), nn.BatchNorm2d(ch * 8), nn.ReLU(),
-            nn.AvgPool2d(scale, scale), # 4
+            nn.MaxPool2d(scale, scale), # 4
             nn.Conv2d(ch * 8, ch * 8, 3, 1, 1, bias=False), nn.BatchNorm2d(ch * 8), nn.ReLU(),
-            nn.AvgPool2d(scale, scale)
+            nn.MaxPool2d(scale, scale), # 2
         )
                            
         self.z_loc_layer = nn.Sequential(
-            nn.Linear(ch * 8 * 2 * 2, 512),
+            nn.Linear(self.features_output, 256),
             Swish(),
             nn.Dropout(p=0.1),
-            nn.Linear(512, z_dim))
+            nn.Linear(256, z_dim))
         
         self.z_scale_layer = nn.Sequential(
-            nn.Linear(ch * 8 * 2 * 2, 512),
+            nn.Linear(self.features_output, 256),
             Swish(),
             nn.Dropout(p=0.1),
-            nn.Linear(512, z_dim))
-        self.z_dim = z_dim
+            nn.Linear(256, z_dim))
+        
 
     def forward(self, image):
         hidden = self.features(image)
-        hidden = hidden.view(-1, self.ch * 8 * 2 * 2)
+        hidden = hidden.view(-1, self.features_output)
         z_loc = self.z_loc_layer(hidden)
         z_scale = torch.exp(self.z_scale_layer(hidden))
         return z_loc, z_scale
     
     
 class ImageDecoder(nn.Module):
-    def __init__(self, z_dim=512, ch=32, scale=2):
+    def __init__(self, z_dim=64, ch=64, scale=2):
         super(ImageDecoder, self).__init__()
         
         self.ch = ch
@@ -79,74 +81,67 @@ class ImageDecoder(nn.Module):
             Swish())
         
         self.hallucinate = nn.Sequential(
-            nn.ConvTranspose2d(ch * 8, ch * 8, 3, 1, 1, bias=False), nn.BatchNorm2d(ch * 8), nn.ReLU(),
+            nn.Conv2d(ch * 8, ch * 8, 3, 1, 1, bias=False), nn.BatchNorm2d(ch * 8), nn.ReLU(),
             nn.Upsample(scale_factor = scale, mode = "nearest"),
-            nn.ConvTranspose2d(ch * 8, ch * 4, 3, 1, 1, bias=False), nn.BatchNorm2d(ch * 4), nn.ReLU(),
+            nn.Conv2d(ch * 8, ch * 4, 3, 1, 1, bias=False), nn.BatchNorm2d(ch * 4), nn.ReLU(),
             nn.Upsample(scale_factor = scale, mode = "nearest"),
-            nn.ConvTranspose2d(ch * 4, ch * 2, 3, 1, 1, bias=False), nn.BatchNorm2d(ch * 2), nn.ReLU(),
+            nn.Conv2d(ch * 4, ch * 2, 3, 1, 1, bias=False), nn.BatchNorm2d(ch * 2), nn.ReLU(),
             nn.Upsample(scale_factor = scale, mode = "nearest"),
-            nn.ConvTranspose2d(ch * 2, ch, 3, 1, 1, bias=False), nn.BatchNorm2d(ch), nn.ReLU(),
+            nn.Conv2d(ch * 2, ch, 3, 1, 1, bias=False), nn.BatchNorm2d(ch), nn.ReLU(),
             nn.Upsample(scale_factor = scale, mode = "nearest"),
-            nn.ConvTranspose2d(ch, 3, 3, 1, 1),
+            nn.Conv2d(ch, 3, 3, 1, 1),
             nn.Upsample(scale_factor = scale, mode = "nearest"))
 
     def forward(self, z):
         z = self.upsample(z)
         z = z.view(-1, self.ch * 8, 2, 2)
-        image = self.hallucinate(z) # this is the image
+        image = self.hallucinate(z)
         return image 
 
 
 class EmotionEncoder(nn.Module):
     def __init__(self, z_dim, input_dim, use_cuda=True):
         super(EmotionEncoder, self).__init__()
-        self.net = nn.Linear(input_dim, 512)
+        self.input_dim = input_dim
+        self.net = nn.Linear(input_dim, 256)
         
         self.z_loc_layer = nn.Sequential(
-            nn.Linear(512, 512),
+            nn.Linear(256, 256),
             Swish(),
-            nn.Linear(512, z_dim))
+            nn.Linear(256, z_dim))
         
         self.z_scale_layer = nn.Sequential(
-            nn.Linear(512, 512),
+            nn.Linear(256, 256),
             Swish(),
-            nn.Linear(512, z_dim))
+            nn.Linear(256, z_dim))
         self.z_dim = z_dim
 
-    def forward(self, emocat):
-        hidden = self.net(emocat)
+    def forward(self, emotion):
+        emotion = torch.stack([F.one_hot(emo, self.input_dim) for emo in emotion])
+        hidden = self.net(emotion.to(torch.float64))
         z_loc = self.z_loc_layer(hidden)
         z_scale = torch.exp(self.z_scale_layer(hidden))
         return z_loc, z_scale
 
 
 class EmotionDecoder(nn.Module):
-    def __init__(self, z_dim, input_dim):
+    def __init__(self, z_dim, output_dim):
         super(EmotionDecoder, self).__init__()
         self.net = nn.Sequential(
-            nn.Linear(z_dim, 512),
-            Swish())
-        
-        self.emotion_loc_layer = nn.Sequential(
-            nn.Linear(512, 512),
+            nn.Linear(z_dim, 256),
             Swish(),
-            nn.Linear(512, input_dim))
+            nn.Linear(256, output_dim),
+            nn.Softmax(dim=0)
+        )
         
-        self.emotion_scale_layer = nn.Sequential(
-            nn.Linear(512, 512),
-            Swish(),
-            nn.Linear(512, input_dim))
-
     def forward(self, z):
-        hidden = self.net(z)
-        emotion_loc = self.emotion_loc_layer(hidden)
-        emotion_scale = torch.exp(self.emotion_scale_layer(hidden))
-        return emotion_loc, emotion_scale
+        return self.net(z)
 
 
 class MVAE(nn.Module):
     def __init__(self, z_dim, emotion_dim, img_size=128, ch_size=64, use_cuda=True):
         super(MVAE, self).__init__()
+        
         self.z_dim = z_dim
         self.img_size = img_size
         self.experts = ProductOfExperts()
@@ -173,8 +168,8 @@ class MVAE(nn.Module):
         
         with pyro.plate("data"):      
             # sample the latent z from the (constant) prior, z ~ Normal(0,I)
-            #z_prior_loc  = torch.zeros(size=[batch_size, self.z_dim])
-            #z_prior_scale = torch.exp(torch.zeros(size=[batch_size, self.z_dim]))     
+            #z_loc  = torch.zeros(size=[batch_size, self.z_dim])
+            #z_scale = torch.exp(torch.zeros(size=[batch_size, self.z_dim]))     
             
             z_loc = torch.zeros(torch.Size((1, batch_size, self.z_dim))) + 0.5
             z_scale = torch.ones(torch.Size((1, batch_size, self.z_dim))) * 0.1
@@ -188,19 +183,19 @@ class MVAE(nn.Module):
 
             # decode the latent code z
             img_loc = self.image_decoder.forward(z)
-            emotion_loc, emotion_scale = self.emotion_decoder.forward(z)
+            emo_probs = self.emotion_decoder.forward(z)
             
             # score against actual images
             if images is not None:
                 with poutine.scale(scale=self.LAMBDA_IMAGES):
                     pyro.sample("obs_img", dist.Bernoulli(img_loc), obs=images)
-            
+                    
             # score against actual emotion
             if emotions is not None:
                 with poutine.scale(scale=self.LAMBDA_RATINGS):
-                    pyro.sample("obs_emotion", dist.Normal(emotion_loc, emotion_scale), obs=emotions)
+                    pyro.sample("obs_emotion", dist.Categorical(probs=emo_probs), obs=emotions)
 
-            return img_loc, emotion_loc
+            return img_loc, emo_probs
         
     def guide(self, images=None, emotions=None, annealing_beta=1.0):
         pyro.module("mvae", self)
@@ -231,6 +226,7 @@ class MVAE(nn.Module):
                 z_scale = torch.cat((z_scale, emotion_z_scale.unsqueeze(0)), dim=0)
             
             z_loc, z_scale = self.experts(z_loc, z_scale)
+            
             # sample the latent z
             with poutine.scale(scale=annealing_beta):
                 pyro.sample("z", dist.Normal(z_loc, z_scale))
@@ -252,8 +248,8 @@ class MVAE(nn.Module):
         elif emotions is not None:
             batch_size = emotions.size(0)
             
-        z_loc = torch.zeros(torch.Size((1, BATCH_SIZE, self.z_dim))) + 0.5
-        z_scale = torch.ones(torch.Size((1, BATCH_SIXE, self.z_dim))) * 0.1
+        z_loc = torch.zeros(torch.Size((1, batch_size, self.z_dim))) + 0.5
+        z_scale = torch.ones(torch.Size((1, batch_size, self.z_dim))) * 0.1
         
         if self.use_cuda:
             z_loc, z_scale = z_loc.cuda(), z_scale.cuda()
@@ -271,35 +267,27 @@ class MVAE(nn.Module):
         z_loc, z_scale = self.experts(z_loc, z_scale)
         return z_loc, z_scale      
 
-    
     def reconstruct_img(self, images):
         # encode image x
         z_loc, z_scale = self.image_encoder(images)
         z = dist.Normal(z_loc, z_scale).sample()
         img_loc = self.image_decoder.forward(z)
         return img_loc
-    
-    
+ 
     def reconstruct_img_nosample(self, images):
         # encode image x
         z_loc, z_scale = self.image_encoder(images)
         img_loc = self.image_decoder.forward(z_loc)
         return img_loc
     
-    
     def emotion_classifier(self, images):
         z_loc, z_scale = self.image_encoder(images)
         z = dist.Normal(z_loc, z_scale).sample()
-        emotion_loc, emotion_scale = self.emotion_decoder.forward(z)
-        return dist.Normal(emotion_loc, emotion_scale).sample()
+        return self.emotion_decoder.forward(z)
  
-
     def emoToZ(self, emotion):
         z_loc, z_scale = self.emotion_encoder.forward(emotion)
         return dist.Normal(z_loc, z_scale).sample()
 
-    
     def zToEmo(self, z):
-        emotion_loc, emotion_scale = self.emotion_decoder.forward(z)
-        return dist.Normal(emotion_loc, emotion_scale).sample()
-        
+        return self.emotion_decoder.forward(z)
