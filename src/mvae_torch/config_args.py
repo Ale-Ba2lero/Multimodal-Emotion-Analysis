9e7caaea6943c9562cf4e260e829439fe345afd5
Alessandro Ballerini <alessandroballerini95@gmail.com>
2022-07-07 10:50:32 +0200

    Images generation

--- src/mvae_torch/config_args.py
+++ src/mvae_torch/config_args.py
@@ -31,7 +31,7 @@ class ConfigTrainArgs:
     learning_rate= 0.0001 
     alpha=1.0
     static_annealing_beta= 1e-6
-    num_epochs= 50
+    num_epochs= 45
     num_workers= 32
     optim_betas= [ 0.95, 0.98 ]
     seed= 100
@@ -43,7 +43,7 @@ class ConfigTrainArgs:
 BEST POE 55-92 
 L2 - L2
 {'cat_dim': 8, 'au_dim': 18, 'latent_space_dim': 25, 'hidden_dim': 128, 'num_filters': 32, 'modes': {'au': True, 'face': None, 'emotion': True}, 'au_weight': 10, 'emotion_weight': 0.001, 'expert_type': 'poe', 'use_cuda': True}
-{'learning_rate': 0.0001, 'beta':1e-6, 'optim_betas': [0.95, 0.98], 'num_epochs': 50, 'batch_size': 256}
+{'learning_rate': 0.0001, 'alpha'=1.0, 'beta':1e-6, 'optim_betas': [0.95, 0.98], 'num_epochs': 50, 'batch_size': 256}
 
 
 Best trial config: {'batch_size': 128, 'z_dim': 25, 'hidden_dim': 512, 'lr': 0.001, 'alpha': 0.1, 'beta': 0.1, 'au_weight': 45751.87811347376, 'emotion_weight': 0.002556536731523064}
